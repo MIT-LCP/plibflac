5c44728065312aa513f9db1bcf745124a70e07f1
Benjamin Moody <benjaminmoody@gmail.com>
2024-07-22 10:57:46 -0400

    setup.py: extract version number from pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
+import re
+
 from setuptools import Extension, setup
+
+with open('pyproject.toml') as f:
+    _version = re.search('^version *= *"(.*?)"', f.read(), re.M)[1]
 
 setup(
     name="plibflac",
+    version=_version,
     package_dir={'': 'src'},
     packages=["plibflac"],
     ext_modules=[
