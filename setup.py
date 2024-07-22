bdf1f148bf10ee716bc577d9b5687476802dc4d6
Benjamin Moody <benjaminmoody@gmail.com>
2024-07-22 11:40:09 -0400

    setup.py: build using the stable ABI if possible.
    In order to build wheel packages that are compatible with multiple
    CPython versions, several things are needed:
    
    - Py_LIMITED_API must be defined when compiling; this ensures only
      stable functions and macros are exposed
    
    - py_limited_api=True must be set for the Extension; this ensures the
      module has a version-independent filename (*.abi3.so)
    
    - the py_limited_api option must be set for the bdist_wheel command;
      this ensures the resulting wheel file has a forward-compatible
      filename (*-cp*-abi3-*.whl)

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python3
 
+import platform
 import re
+import sys
 
 from setuptools import Extension, setup
 
+################################################################
+
 with open('pyproject.toml') as f:
     _version = re.search('^version *= *"(.*?)"', f.read(), re.M)[1]
+
+################################################################
+
+_stable_abi = (3, 5)
+if (platform.python_implementation() == 'CPython'
+        and sys.version_info >= _stable_abi):
+    _define_macros = [('Py_LIMITED_API', '0x%02x%02x0000' % _stable_abi)]
+    _py_limited_api = True
+    _bdist_wheel_options = {'py_limited_api': 'cp%d%d' % _stable_abi}
+else:
+    _define_macros = []
+    _py_limited_api = False
+    _bdist_wheel_options = {}
+
+################################################################
 
 setup(
     name="plibflac",
@@ -17,6 +36,11 @@ setup(
             name="_plibflac",
             sources=["src/_plibflacmodule.c"],
             libraries=["FLAC"],
+            define_macros=_define_macros,
+            py_limited_api=_py_limited_api,
         ),
     ],
+    options={
+        'bdist_wheel': _bdist_wheel_options,
+    },
 )
